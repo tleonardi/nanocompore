6fe3384c1da4438ccf79d2e799be7a2fa9e97030
Tommaso Leonardi <tom@tleo.io>
2018-08-04 10:53:21 +0100

    Refactored to class

--- main.py
+++ main.py
@@ -4,101 +4,112 @@ from queue import Queue
 import threading
 import mmap
 
-logging.basicConfig(level=logging.DEBUG)
 
-# setting up logger
-logger = logging.getLogger("Logger")
+logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+logger = logging.getLogger('simple_example')
 logger.setLevel(logging.DEBUG)
-console = logging.StreamHandler()
+logger.setLevel(logging.INFO)
 
 
-def mmap_readline(file, sep="\t"):
-    with open(file, "r+b") as f:
-        m = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
-        while True:
-            line=m.readline()
-            if line == b'': break
-            yield line.decode().split(sep)[0:7]
+class events_combinator(object):
+    """ Produce usefule results """
+    
+    def __init__(self):
+        self.__main_queue = Queue(maxsize=20)
+        self.results=[] 
+        self.data_f1=dict()
+        self.data_f2=dict()
+        self.processed=set()
+        self.n_consumer_threads=3
+        
+        #self.file1="/mnt/home1/kouzarides/tl344/projects/nanopore_7SK/analysis_wt/nanopolish/events.txt"
+        self.file1="events.txt"
+        self.file2="events2.txt"
+        #self.file2="/mnt/home1/kouzarides/tl344/projects/nanopore_7SK/analysis_wt/nanopolish/events.txt"
+        
+        self.reader1 = threading.Thread(name="reader1", target=self.__parse_events_file, args=(self.file1, self.data_f1,))
+        self.reader2 = threading.Thread(name="reader2", target=self.__parse_events_file, args=(self.file2, self.data_f2,))
+        
+        consumers=[]
+        for i in range(self.n_consumer_threads):
+            consumers.append(threading.Thread(name="consumer%s"%i, target=self.__consumer))
+        
+        self.reader1.start()
+        self.reader2.start()
+        
+        for c in consumers:
+            c.start()
+        
+        self.reader1.join()
+        self.reader2.join()
+        self.__main_queue.join()
+        for r in self.results:
+            print(r)
+        for _ in consumers:
+            self.__main_queue.put(None)
 
-def read_file2(file, main_queue, data):
-    tx="contig"
-    res=[]
-    for line in mmap_readline(file):
-        if line[0]==tx:
-            res.append(line)
-            tx=line[0]
-            logger.debug("Adding line to (%s)" % (tx))
-        else:
-            data[tx]=res
-            main_queue.put(tx)
-            res=line
-            tx=line[0]
-            logger.debug("Finished processing (%s), (%s) lines" % (tx, len(data[tx])))
-       
-def read_file(file, main_queue, data):
-    logger.debug("Entered read_file with (%s)" % file)
-    #for i in range(1000000000000):
-    i=0
-    for i in range(5):
-        i+=1
-        sleep(0.3)
-        res = {'Name':"TX(%s)"%i, 'Data':[["Name", "data", "data"]]}
-        data[res['Name']] = res['Data']
-        main_queue.put(res["Name"])
-        logger.debug("read_file: Added (%s) to Q. Q has (%s) items." % (res["Name"], main_queue.qsize()))
-       
-def process_tx(tx, data1, data2):
-    data1.pop(tx)
-    data2.pop(tx)
-    print("processing (%s)"%tx)
-    sleep(10)
-    print("(%s) Done"%tx)
-    results="Done"
-    return results
 
-def consumer(main_queue, results, data1, data2, processed):
-    while True:
-        # Fetch a tx from the queue
-        tx =  main_queue.get()
-        if tx is None:
-            break
-        logger.debug("consumer: picked (%s) from Q. Q has (%s) items." % (tx, main_queue.qsize()))
-        # Check data is in data_1 and data_2, if not put back in main_queue
-        if tx not in processed:
-            if tx in data1.keys() and tx in data2.keys():
-                results.append( process_tx(tx, data1, data2))
-                processed.add(tx)
+    @staticmethod
+    def mmap_readline(file, sep="\t"):
+        """ Iterator for opening files as mem maps """
+        with open(file, "r+b") as f:
+            m = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
+            while True:
+                line=m.readline()
+                if line == b'': break
+                yield line.decode().split(sep)[0:7]
+     
+    def __parse_events_file(self, file, data):
+        """ Parse an events file line by line and extract lines
+        corresponding to the same transcripts. When a transcript
+        is read entirely, add its name to the __main_queue and the
+        the data to data. """
+        tx=""
+        res=[]
+        for line in self.mmap_readline(file):
+            if line[0]==tx:
+                res.append(line)
+                tx=line[0]
+                logger.debug("Processing (%s)" % (tx))
             else:
-                main_queue.put(tx)
-                logger.debug("consumer: Returning (%s) to Q. Q has (%s) items" % (tx, main_queue.qsize()))
-        main_queue.task_done()
+                if len(res)!=0:
+                    # Might be worth making res an object of a dedicated class
+                    data[tx]=res
+                    self.__main_queue.put(tx)
+                logger.debug("Finished processing (%s)" % (tx))
+                res=[line]
+                tx=line[0]
+        data[tx]=res
+        self.__main_queue.put(tx)
+        logger.debug("Finished processing (%s)" % (tx))
 
+    def __consumer(self):
+        while True:
+            # Fetch a tx from the queue
+            tx =  self.__main_queue.get()
+            logger.debug("Picked (%s) from Q. Q has (%s) items." % (tx, self.__main_queue.qsize()))
+            # Terminate consumer is we reach
+            # bottom of the queue
+            if tx is None:
+                break
+            # Check data is in data_1 and data_2, if not put back in __main_queue
+            if tx not in self.processed:
+                if tx in self.data_f1.keys() and tx in self.data_f2.keys():
+                    self.results.append(self.process_tx(self.data_f1[tx], self.data_f2[tx]))
+                    self.processed.add(tx)
+                elif self.reader1.is_alive() or self.reader2.is_alive():
+                    self.__main_queue.put(tx)
+                    logger.debug("Returning (%s) to Q. Q has (%s) items" % (tx, self.__main_queue.qsize()))
+            self.__main_queue.task_done()
 
-
-main_queue = Queue(maxsize=20)
-results=[] 
-data_f1=dict()
-data_f2=dict()
-
-file1="/mnt/home1/kouzarides/tl344/projects/nanopore_7SK/analysis_wt/nanopolish/events.txt"
-file2="/mnt/home1/kouzarides/tl344/projects/nanopore_7SK/analysis_wt/nanopolish/events.txt"
-
-processed=set()
-reader1 = threading.Thread(name="reader1", target=read_file2, args=(file1, main_queue, data_f1,))
-#reader2 = threading.Thread(name="reader2", target=read_file2, args=(file2, main_queue, data_f2,))
-
-consumers=[]
-for i in range(3):
-    consumers.append(threading.Thread(name="consumer (%s)"%i, target=consumer, args=(main_queue, results, data_f1, data_f2, processed)))
-
-reader1.start()
-#reader2.start()
-
-#for c in consumers:
-    #c.start()
-
-reader1.join()
-#reader2.join()
-print(data_f1)
-for _ in consumers:
-    main_queue.put(None)
+    @staticmethod
+    def process_tx(data1, data2):
+        if data1[0][0] != data2[0][0]:
+            print("Error")
+        else: tx=data1[0][0]
+        logger.info("Processing %s" % (tx))
+        return("Tx (%s) done. Len f1: (%s) - Len f2: (%s)" % (tx, len(data1), len(data2)))
+        logger.info("Done processing %s" % (tx))
+    
+if __name__ == '__main__':
+    d = events_combinator()
