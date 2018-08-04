55b50b2fb4b449e70199c40fb29d4b7887d6d69e
Tommaso Leonardi <tom@tleo.io>
2018-08-04 08:53:34 +0100

    First working threaded implementation

--- main.py
+++ main.py
@@ -1,89 +1,104 @@
-import asyncio
 import logging
 from time import sleep
-from aiofile import AIOFile, LineReader
+from queue import Queue
+import threading
+import mmap
 
 logging.basicConfig(level=logging.DEBUG)
 
 # setting up logger
-logger = logging.getLogger("MyLogger")
+logger = logging.getLogger("Logger")
 logger.setLevel(logging.DEBUG)
 console = logging.StreamHandler()
-logger.addHandler(console)
-MAX_CONNECTION = 1000
 
 
-        
-async def read_file(file, main_queue, data):
+def mmap_readline(file, sep="\t"):
+    with open(file, "r+b") as f:
+        m = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
+        while True:
+            line=m.readline()
+            if line == b'': break
+            yield line.decode().split(sep)[0:7]
+
+def read_file2(file, main_queue, data):
+    tx="contig"
+    res=[]
+    for line in mmap_readline(file):
+        if line[0]==tx:
+            res.append(line)
+            tx=line[0]
+            logger.debug("Adding line to (%s)" % (tx))
+        else:
+            data[tx]=res
+            main_queue.put(tx)
+            res=line
+            tx=line[0]
+            logger.debug("Finished processing (%s), (%s) lines" % (tx, len(data[tx])))
+       
+def read_file(file, main_queue, data):
     logger.debug("Entered read_file with (%s)" % file)
     #for i in range(1000000000000):
     i=0
     for i in range(5):
         i+=1
-        asyncio.sleep(10)
+        sleep(0.3)
         res = {'Name':"TX(%s)"%i, 'Data':[["Name", "data", "data"]]}
         data[res['Name']] = res['Data']
-        await main_queue.put(res["Name"])
+        main_queue.put(res["Name"])
         logger.debug("read_file: Added (%s) to Q. Q has (%s) items." % (res["Name"], main_queue.qsize()))
        
 def process_tx(tx, data1, data2):
     data1.pop(tx)
     data2.pop(tx)
     print("processing (%s)"%tx)
-    sleep(1)
+    sleep(10)
     print("(%s) Done"%tx)
     results="Done"
     return results
 
-async def consumer(main_queue, results, data1, data2):
+def consumer(main_queue, results, data1, data2, processed):
     while True:
-        logger.debug("Consumer - data1 has (%s) elements - data2 has (%s) elements." % (len(data1.keys()), len(data2.keys())))
         # Fetch a tx from the queue
-        tx = await main_queue.get()
+        tx =  main_queue.get()
+        if tx is None:
+            break
         logger.debug("consumer: picked (%s) from Q. Q has (%s) items." % (tx, main_queue.qsize()))
         # Check data is in data_1 and data_2, if not put back in main_queue
-
-        if tx in data1.keys() and tx in data2.keys():
-            results.append(await process_tx(tx, data1, data2))
-            main_queue.task_done()
-        else:
-            await main_queue.put(tx)
-            #logger.debug("consumer: Returning (%s) to Q. Q has (%s) items" % (tx, main_queue.qsize()))
-            logger.debug(data1.keys())
-            logger.debug(data2.keys())
-
-
-async def consume_files(file1, file2, n=5):
-    main_queue, results = asyncio.Queue(maxsize=20), []
-    
-    # we init the consumers, as the queues are empty at first,
-    # they will be blocked on the main_queue.get()
-    data_f1=dict()
-    data_f2=dict()
-    consumers = [asyncio.ensure_future(consumer(main_queue, results, data_f1, data_f2)) for _ in range(n)]
-
-    producer1 = await read_file(file1, main_queue, data_f1)
-    producer2 = await read_file(file2, main_queue, data_f2)
-    #producers = [asyncio.ensure_future(read_file(file1, main_queue, data_f1)), read_file(file2, main_queue, data_f1)]
-    logger.debug("Here")
-
-    # wait for all item's inside the
-    # main_queue to get task_done
-    await main_queue.join()
-    # cancel all coroutines
-    #for consumer_future in consumers:
-    #    consumer_future.cancel()
-    return results
+        if tx not in processed:
+            if tx in data1.keys() and tx in data2.keys():
+                results.append( process_tx(tx, data1, data2))
+                processed.add(tx)
+            else:
+                main_queue.put(tx)
+                logger.debug("consumer: Returning (%s) to Q. Q has (%s) items" % (tx, main_queue.qsize()))
+        main_queue.task_done()
 
 
-async def run():
-    # we init more connectors to get better performance
-    file1="/mnt/home1/kouzarides/tl344/projects/nanopore_7SK/analysis_kd/nanopolish/events.txt"
-    file2="/mnt/home1/kouzarides/tl344/projects/nanopore_7SK/analysis_wt/nanopolish/events.txt"
-    results = await consume_files(file1, file2, 100)
-    print(results)
 
+main_queue = Queue(maxsize=20)
+results=[] 
+data_f1=dict()
+data_f2=dict()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run())
+file1="/mnt/home1/kouzarides/tl344/projects/nanopore_7SK/analysis_wt/nanopolish/events.txt"
+file2="/mnt/home1/kouzarides/tl344/projects/nanopore_7SK/analysis_wt/nanopolish/events.txt"
 
+processed=set()
+reader1 = threading.Thread(name="reader1", target=read_file2, args=(file1, main_queue, data_f1,))
+#reader2 = threading.Thread(name="reader2", target=read_file2, args=(file2, main_queue, data_f2,))
+
+consumers=[]
+for i in range(3):
+    consumers.append(threading.Thread(name="consumer (%s)"%i, target=consumer, args=(main_queue, results, data_f1, data_f2, processed)))
+
+reader1.start()
+#reader2.start()
+
+#for c in consumers:
+    #c.start()
+
+reader1.join()
+#reader2.join()
+print(data_f1)
+for _ in consumers:
+    main_queue.put(None)
