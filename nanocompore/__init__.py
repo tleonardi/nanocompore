b629fb4f8dc8f374d432f215a91a6c096766f742
a-slide <aleg@ebi.ac.uk>
2018-09-27 15:09:59 +0100

    Update version number

--- nanocompore/__init__.py
+++ nanocompore/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Define self package variable
-__version__ = "0.1.4.1"
+__version__ = "0.1.4.2"
 
 description = 'Software package that identifies raw signal changes between two conditions from https://github.com/jts/nanopolish resquiggled dRNA-Seq data.'
 long_description = """"""
