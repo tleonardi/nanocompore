64abf4d641020df4fa28f88186fa94c264aeb429
a-slide <aleg@ebi.ac.uk>
2018-10-02 17:10:22 +0100

    Add plot functionalities + doc

--- nanocompore/SampCompDB.py
+++ nanocompore/SampCompDB.py
@@ -7,6 +7,7 @@ import shelve
 
 # Third party
 from pyfaidx import Fasta
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as pl
 import seaborn as sns
@@ -16,12 +17,17 @@ from nanocompore.common import counter_to_str, access_file, NanocomporeError
 
 #~~~~~~~~~~~~~~MAIN CLASS~~~~~~~~~~~~~~#
 class SampCompDB (object):
-    """ Wrapper over the shelve db"""
+    """ Wrapper over the result shelve SampComp """
 
     #~~~~~~~~~~~~~~FUNDAMENTAL METHODS~~~~~~~~~~~~~~#
     def __init__(self, db_fn, fasta_fn):
+        """
+        Import a shelve db and a fasta reference file. Automatically returned by SampComp
+        Can also be manually created from an existing shelve db output
+        db_fn: Path where to write the result database
+        fasta_fn: Path to a fasta file corresponding to the reference used for read alignemnt
+        """
         # Check file
-
         for fn in (db_fn, fasta_fn):
             if not access_file (fn):
                 raise NanocomporeError("Cannot access file {}".format(fn))
@@ -63,11 +69,40 @@ class SampCompDB (object):
                 return None
 
     #~~~~~~~~~~~~~~PUBLIC METHODS~~~~~~~~~~~~~~#
-    def plot_pvalues (self, ref_id, start=None, end=None, pvalue_name="pvalue_median"):
+
+    ################################### TO DO ##################################
+    def to_bed (self, output_fn):
         pass
 
-    def plot_signal (self, ref_id, start, end):
-        ref_fasta = self._fasta[ref_id]
+    def to_some_other_kind_of_text_output (self, output_fn):
+        pass
+
+    def list_most_significant_positions (self, n=10):
+        pass
+
+    def list_most_significant_references (self, n=10):
+        pass
+
+    ############################################################################
+
+    def plot_signal (self, ref_id, start, end, figsize=(30,10), palette="Set2", plot_style="ggplot"):
+        """
+        Plot the dwell time and median intensity distribution position per positon in a split violin plot representation.
+        It is pointless to plot more than 50 positions at once as it becomes hard to distiguish
+        ref_id: Valid reference id name in the database
+        start: Start coordinate (Must be higher or equal to 0)
+        end: End coordinate (included) (must be lower or equal to the reference length)
+        figsize: length and heigh of the output plot. Default=(30,10)
+        palette: Colormap. Default="Set2"
+            see https://matplotlib.org/users/colormaps.html, https://matplotlib.org/examples/color/named_colors.html
+        plot_style: Matplotlib plotting style
+            . See https://matplotlib.org/users/style_sheets.html
+        """
+
+        try:
+            ref_fasta = self._fasta [ref_id]
+        except KeyError:
+            raise NanocomporeError ("Reference id not present in result database")
         if start > end:
             raise NanocomporeError ("End coordinate has to be higher or equal to start")
         if start < 0:
@@ -98,10 +133,90 @@ class SampCompDB (object):
 
         # Cast collected results to dataframe
         df = pd.DataFrame (l)
-        fig, axes = pl.subplots(2, 1, figsize=(25,10))
-        for variable, ax in zip (("median", "dwell"), axes):
-            _  = sns.violinplot (x="pos", y=variable, hue="sample", data=df, split=True, ax=ax, inner="quartile", bw=0.75, linewidth=1)
-            _ = ax.set_xticklabels(x_lab)
-            _ = ax.set_xlabel ("")
+        if df.empty:
+            raise NanocomporeError ("No data available for the selected interval")
 
-        return (fig, axes)
+        # Define ploting style
+        with pl.style.context (plot_style):
+            # Plot dwell and median
+            fig, axes = pl.subplots(2, 1, figsize=figsize)
+             ######### Add model value for median ?
+            _ = sns.violinplot (x="pos", y="median", hue="sample", data=df, split=True, ax=axes[0], inner="quartile", bw=0.75, linewidth=1, palette=palette)
+            _ = sns.violinplot (x="pos", y="dwell", hue="sample", data=df, split=True, ax=axes[1], inner="quartile", bw=0.75, linewidth=1, palette=palette)
+
+            # Adjust display
+            _ = axes[0].set_title (ref_id)
+            _ = axes[0].get_xaxis().set_visible(False)
+            _ = axes[0].set_ylabel ("Median Intensity")
+            _ = axes[1].set_xticklabels (x_lab)
+            _ = axes[1].set_ylabel ("Dwell Time")
+            _ = axes[1].set_xlabel ("Reference position")
+
+            pl.tight_layout()
+            return (fig, axes)
+
+    def plot_pvalue (self, ref_id, start=None, end=None, threshold=0.01, figsize=(30,10), palette="Set2", plot_style="ggplot"):
+        """
+        Plot pvalues per position (by default plot all fields starting by "pvalue")
+        It is pointless to plot more than 50 positions at once as it becomes hard to distiguish
+        ref_id: Valid reference id name in the database
+        start: Start coordinate. Default=0
+        end: End coordinate (included). Default=reference length
+        figsize: length and heigh of the output plot. Default=(30,10)
+        palette: Colormap. Default="Set2"
+            see https://matplotlib.org/users/colormaps.html, https://matplotlib.org/examples/color/named_colors.html
+        plot_style: Matplotlib plotting style. Default="ggplot"
+            . See https://matplotlib.org/users/style_sheets.html
+        """
+        try:
+            ref_fasta = self._fasta [ref_id]
+        except KeyError:
+            raise NanocomporeError ("Reference id not present in result database")
+
+        # Define start, end if not given
+        if not start:
+            start = 0
+        if not end:
+            end = len (ref_fasta)
+        # Check start end
+        if start > end:
+            raise NanocomporeError ("End coordinate has to be higher or equal to start")
+        if start < 0:
+            raise NanocomporeError ("Coordinates have to be higher that 0")
+        if end > len(ref_fasta):
+            raise NanocomporeError ("Coordinates have to be lower than the ref_id sequence length ({})".format(len(ref_fasta)))
+
+        # Parse line position per position
+        d = OrderedDict ()
+        ref_pos_dict = self [ref_id]
+        for pos in range (start, end+1):
+            # Collect results for position
+            res_dict = OrderedDict ()
+            #res_dict ["pos"] = pos
+            if pos in ref_pos_dict:
+                for k,v in ref_pos_dict[pos].items():
+                    if k.startswith ("pvalue"): # Get every fields starting with "pvalue"
+                        res_dict [k] = v
+            d[pos] = res_dict
+
+        # Cast collected results to dataframe
+        df = pd.DataFrame.from_dict(d, orient="index")
+        if df.empty:
+            raise NanocomporeError ("No data available for the selected interval")
+
+        # filling missing values and log transform the data
+        df.fillna(1, inplace=True)
+        df = -np.log(df)
+
+        # Define ploting style
+        with pl.style.context (plot_style):
+            fig, ax = pl.subplots(figsize=figsize)
+            _ = sns.lineplot(data=df, palette=palette, ax=ax)
+            _ = ax.axhline (y=-np.log(threshold), color="grey", linestyle=":", label="pvalue={}".format(threshold))
+            _ = ax.legend ()
+            _ = ax.set_ylabel ("-log (pvalue)")
+            _ = ax.set_xlabel ("Reference position")
+            _ = ax.set_title (ref_id)
+
+            pl.tight_layout()
+            return (fig, ax)
