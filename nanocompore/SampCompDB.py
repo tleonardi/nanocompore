f6ff8f120596ca2518ef724e863a57704f1fda3a
a-slide <aleg@ebi.ac.uk>
2018-10-15 23:29:11 +0100

    Update plot_signal and add plot_coverage + handle __metadata exception

--- nanocompore/SampCompDB.py
+++ nanocompore/SampCompDB.py
@@ -35,22 +35,25 @@ class SampCompDB (object):
             if not access_file (fn):
                 raise NanocomporeError("Cannot access file {}".format(fn))
 
-        # Try to get ref_id list from shelve db
+        # Try to get ref_id list and metadata from shelve db
         try:
             with shelve.open (db_fn, flag='r') as db:
-                self.ref_id_list = list (db.keys())
-                try: 
+
+                # Try to get metedata from db
+                try:
                     metadata = db['__metadata']
+                    self._comparison_method=metadata['comparison_method']
+                    self._sequence_context=metadata['sequence_context']
                 except KeyError:
                     raise NanocomporeError("The result database does not contain metadata")
-                self._comparison_method=metadata['comparison_method']
-                self._sequence_context=metadata['sequence_context']
-                self.ref_id_list.remove('__metadata')
+
+                # Try to load read_ids
+                self.ref_id_list = [k for k in db.keys() if k!='__metadata']
                 if not self.ref_id_list:
                     raise NanocomporeError("The result database is empty")
-                self._db_fn = db_fn
         except:
             raise NanocomporeError("The result database cannot be opened")
+        self._db_fn = db_fn
 
         # Try to open Fasta file
         try:
@@ -64,23 +67,22 @@ class SampCompDB (object):
 
     #~~~~~~~~~~~~~~MAGIC METHODS~~~~~~~~~~~~~~#
     def __len__ (self):
-        return len (self.ref_id_list)
+        return len (self.ref_id_list)-1
 
     def __iter__ (self):
         with shelve.open (self._db_fn, flag = "r") as db:
             for k, v in db.items():
-                yield (k,v)
+                if not k == '__metadata':
+                    yield (k, v)
 
     def __getitem__(self, items):
         with shelve.open (self._db_fn, flag = "r") as db:
             if items in db:
                 return db[items]
             else:
-                return None
+                raise KeyError ("Item not found in the database")
 
     #~~~~~~~~~~~~~~PUBLIC METHODS~~~~~~~~~~~~~~#
-
-    ################################### TO DO ##################################
     def save_to_bed (self, output_fn, bedgraph=False, pvalue_field=None, pvalue_thr=0.01, sequence_context=0, convert=None, assembly=None):
         """Saves the results object to BED6 format.
             bedgraph: save file in bedgraph format instead of bed
@@ -118,8 +120,6 @@ class SampCompDB (object):
                         line=line.translateChr(assembly=assembly, target="ens", patches=True)
                     bed_file.write("%s %s %s %s\n" % (line.chr, line.start, line.end, line.score))
 
-
-
     def calculate_results(self, bed_fn=None, adjust=True, methods=None):
         # Compose a lists with the name of the results
         tests=[]
@@ -141,7 +141,7 @@ class SampCompDB (object):
             c=str(self._sequence_context)
             tests+=[t+"_context="+c for t in tests]
 
-        # We open the DB rather that calling __getitem__ to avoid 
+        # We open the DB rather that calling __getitem__ to avoid
         # opening and closing the file for every transcript
         with shelve.open(self._db_fn, flag = "r") as db:
             df = pd.DataFrame([dict({a:b for a,b in v.items() if a in tests}, ref=ref_id, pos=k)  for ref_id, rec in db.items() for k,v in rec.items() if ref_id!="__metadata" ]).fillna(1)
@@ -164,12 +164,11 @@ class SampCompDB (object):
             df['chr'] = df.apply(lambda row: bed_annot[row['ref']].chr,axis=1)
             df['strand'] = df.apply(lambda row: bed_annot[row['ref']].strand,axis=1)
             df=df[['ref', 'pos', 'chr', 'strand', 'genomicPos']+tests]
+
         if adjust:
             for col in tests:
                 df['adjusted_'+col] = multipletests(df[col], method="fdr_bh")[1]
         self.results = df
-    
-
 
     def list_most_significant_positions (self, n=10):
         pass
@@ -177,81 +176,11 @@ class SampCompDB (object):
     def list_most_significant_references (self, n=10):
         pass
 
-    ############################################################################
-
-    def plot_signal (self, ref_id, start, end, figsize=(30,10), palette="Set2", plot_style="ggplot"):
-        """
-        Plot the dwell time and median intensity distribution position per positon in a split violin plot representation.
-        It is pointless to plot more than 50 positions at once as it becomes hard to distiguish
-        ref_id: Valid reference id name in the database
-        start: Start coordinate (Must be higher or equal to 0)
-        end: End coordinate (included) (must be lower or equal to the reference length)
-        figsize: length and heigh of the output plot. Default=(30,10)
-        palette: Colormap. Default="Set2"
-            see https://matplotlib.org/users/colormaps.html, https://matplotlib.org/examples/color/named_colors.html
-        plot_style: Matplotlib plotting style
-            . See https://matplotlib.org/users/style_sheets.html
-        """
-
-        try:
-            ref_fasta = self._fasta [ref_id]
-        except KeyError:
-            raise NanocomporeError ("Reference id not present in result database")
-        if start > end:
-            raise NanocomporeError ("End coordinate has to be higher or equal to start")
-        if start < 0:
-            raise NanocomporeError ("Coordinates have to be higher that 0")
-        if end > len(ref_fasta):
-            raise NanocomporeError ("Coordinates have to be lower than the ref_id sequence length ({})".format(len(ref_fasta)))
-
-        # Parse line position per position
-        lt = namedtuple ("lt", ["pos", "sample", "median", "dwell"])
-        l = []
-        ref_pos_dict = self[ref_id]
-        for pos in range (start, end+1):
-
-            # Collect results for position
-            if pos in ref_pos_dict:
-                for sample in ("S1", "S2"):
-                    for median, dwell in zip (ref_pos_dict[pos][sample+"_median"], ref_pos_dict[pos][sample+"_dwell"]):
-                        l.append (lt (pos, sample, median, dwell))
-            # If not coverage for position, just fill in with empty values
-            else:
-                for sample in ("S1", "S2"):
-                    l.append (lt (pos, sample, None, None))
-
-        # Create x label including the original sequence and its position
-        x_lab = []
-        for pos, base in zip (range (start, end+1), ref_fasta[start:end+1]):
-            x_lab.append ("{}\n{}".format(pos, base))
-
-        # Cast collected results to dataframe
-        df = pd.DataFrame (l)
-        if df.empty:
-            raise NanocomporeError ("No data available for the selected interval")
-
-        # Define ploting style
-        with pl.style.context (plot_style):
-            # Plot dwell and median
-            fig, axes = pl.subplots(2, 1, figsize=figsize)
-             ######### Add model value for median ?
-            _ = sns.violinplot (x="pos", y="median", hue="sample", data=df, split=True, ax=axes[0], inner="quartile", bw=0.75, linewidth=1, palette=palette)
-            _ = sns.violinplot (x="pos", y="dwell", hue="sample", data=df, split=True, ax=axes[1], inner="quartile", bw=0.75, linewidth=1, palette=palette)
-
-            # Adjust display
-            _ = axes[0].set_title (ref_id)
-            _ = axes[0].get_xaxis().set_visible(False)
-            _ = axes[0].set_ylabel ("Median Intensity")
-            _ = axes[1].set_xticklabels (x_lab)
-            _ = axes[1].set_ylabel ("Dwell Time")
-            _ = axes[1].set_xlabel ("Reference position")
-
-            pl.tight_layout()
-            return (fig, axes)
+    #~~~~~~~~~~~~~~PLOTING METHODS~~~~~~~~~~~~~~#
 
     def plot_pvalue (self, ref_id, start=None, end=None, adjusted_pvalues=True, threshold=0.01, figsize=(30,10), palette="Set2", plot_style="ggplot"):
         """
-        <Plot pvalues per position (by default plot all fields starting by "pvalue")
+        Plot pvalues per position (by default plot all fields starting by "pvalue")
         It is pointless to plot more than 50 positions at once as it becomes hard to distiguish
         ref_id: Valid reference id name in the database
         start: Start coordinate. Default=0
@@ -325,8 +254,147 @@ class SampCompDB (object):
             _ = ax.set_ylabel ("-log (pvalue)")
             _ = ax.set_xlabel ("Reference position")
             _ = ax.set_title (ref_id)
+            _ = ax.set_xlim (start, end)
             if end-start<30:
                 _ = ax.set_xticks(df.index)
                 _ = ax.set_xticklabels(x_lab)
             pl.tight_layout()
             return (fig, ax)
+
+    def plot_signal (self, ref_id, start=None, end=None, figsize=(30,10), colors=["dodgerblue", "salmon"], plot_style="ggplot"):
+        """
+        Plot the dwell time and median intensity distribution position per positon in a split violin plot representation.
+        It is pointless to plot more than 50 positions at once as it becomes hard to distiguish
+        ref_id: Valid reference id name in the database
+        start: Start coordinate (Must be higher or equal to 0)
+        end: End coordinate (included) (must be lower or equal to the reference length)
+        figsize: length and heigh of the output plot. Default=(30,10)
+        palette: Colormap. Default="Set2"
+            see https://matplotlib.org/users/colormaps.html, https://matplotlib.org/examples/color/named_colors.html
+        plot_style: Matplotlib plotting style
+            . See https://matplotlib.org/users/style_sheets.html
+        """
+
+        # Get data
+        ref_data, ref_fasta, start, end = self.__get_plot_data (ref_id, start, end)
+
+        # Parse line position per position
+        lt = namedtuple ("lt", ["pos", "sample", "median", "dwell"])
+        l = []
+        valid_pos=0
+        for pos in np.arange (start, end+1):
+            if pos in ref_data:
+                valid_pos+=1
+                for median, dwell in zip (ref_data[pos]["S1_median"], ref_data[pos]["S1_dwell"]):
+                    l.append (lt (pos, "S1", median, dwell))
+                for median, dwell in zip (ref_data[pos]["S2_median"], ref_data[pos]["S2_dwell"]):
+                    l.append (lt (pos, "S2", median, dwell))
+
+            # If not coverage for position, just fill in with empty values
+            else:
+                for sample in ("S1", "S2"):
+                    l.append (lt (pos, sample, None, None))
+
+        # Check that we found valid position and cast collected results to dataframe
+        if not valid_pos:
+            raise NanocomporeError ("No data available for the selected interval")
+        df = pd.DataFrame (l)
+
+        # Create x label including the original sequence and its position
+        x_lab = []
+        for pos, base in zip (range (start, end+1), ref_fasta[start:end+1]):
+            x_lab.append ("{}\n{}".format(pos, base))
+
+        # Define ploting style
+        with pl.style.context (plot_style):
+            # Plot dwell and median
+            fig, axes = pl.subplots(2, 1, figsize=figsize)
+            _ = sns.violinplot (x="pos", y="median", hue="sample", data=df, split=True, ax=axes[0], inner="quartile", bw=0.75, linewidth=1, palette=colors)
+            _ = sns.violinplot (x="pos", y="dwell", hue="sample", data=df, split=True, ax=axes[1], inner="quartile", bw=0.75, linewidth=1, palette=colors)
+
+            # Adjust display
+            _ = axes[0].set_title (ref_id)
+            _ = axes[0].get_xaxis().set_visible(False)
+            _ = axes[0].set_ylabel ("Median Intensity")
+            _ = axes[1].set_xticklabels (x_lab)
+            _ = axes[1].set_ylabel ("Dwell Time")
+            _ = axes[1].set_xlabel ("Reference position")
+
+            pl.tight_layout()
+            return (fig, axes)
+
+    def plot_coverage (self, ref_id, start=None, end=None, figsize=(30,5), colors=["dodgerblue", "salmon"], plot_style="ggplot"):
+        """
+        Plot pvalues per position (by default plot all fields starting by "pvalue")
+        It is pointless to plot more than 50 positions at once as it becomes hard to distiguish
+        ref_id: Valid reference id name in the database
+        start: Start coordinate. Default=0
+        end: End coordinate (included). Default=reference length
+        figsize: length and heigh of the output plot. Default=(30,10)
+        colors: list of 2 colors
+            see https://matplotlib.org/examples/color/named_colors.html
+        plot_style: Matplotlib plotting style. Default="ggplot"
+            . See https://matplotlib.org/users/style_sheets.html
+        """
+        # Get data
+        ref_data, ref_fasta, start, end = self.__get_plot_data (ref_id, start, end)
+
+
+        # Parse line position per position
+        lt = namedtuple ("lt", ["pos", "S1", "S2"])
+        l = []
+        valid_pos = 0
+        for pos in np.arange (start, end+1):
+            if pos in ref_data:
+                l.append (lt (pos, ref_data[pos]["S1_coverage"], ref_data[pos]["S2_coverage"]))
+                valid_pos+=1
+            else:
+                l.append (lt (pos, None, None))
+
+        # Check that we found valid position and cast collected results to dataframe
+        if not valid_pos:
+            raise NanocomporeError ("No data available for the selected interval")
+        df = pd.DataFrame (l)
+
+        # Define plotting style
+        with pl.style.context (plot_style):
+            fig, ax = pl.subplots(figsize=figsize)
+            _ = ax.plot (df["pos"], df["S1"], label="S1", color=colors[0])
+            _ = ax.plot (df["pos"], df["S2"], label="S2", color=colors[1])
+            _ = ax.set_ylabel ("Coverage")
+            _ = ax.set_xlabel ("Reference position")
+            _ = ax.set_xlim (start, end)
+            _ = ax.set_title (ref_id)
+            _ = ax.legend ()
+            pl.tight_layout()
+            return (fig, ax)
+
+    #~~~~~~~~~~~~~~PRIVATE  METHODS~~~~~~~~~~~~~~#
+    def __get_plot_data (self, ref_id, start, end):
+        """
+        Private function to verify and extract info for the ploting functions
+        """
+
+        # Extract data for ref_id
+        try:
+            ref_data = self[ref_id]
+        except KeyError:
+            raise NanocomporeError ("Reference id not present in result database")
+        # Get corresponding fasta record
+        try:
+            ref_fasta = self._fasta [ref_id]
+        except KeyError:
+            raise NanocomporeError ("Reference id not present in fasta reference database")
+        # Get start and end
+        if not start:
+            start = 0
+        if not end:
+            end = len (ref_fasta)
+        if start > end:
+            raise NanocomporeError ("End coordinate has to be higher or equal to start")
+        if start < 0:
+            raise NanocomporeError ("Coordinates have to be higher that 0")
+        if end > len(ref_fasta):
+            raise NanocomporeError ("Coordinates have to be lower than the ref_id sequence length ({})".format(len(ref_fasta)))
+
+        return (ref_data, ref_fasta, start, end)
