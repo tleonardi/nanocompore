Fixes error related to 0 variance within groups

nanocompore/TxComp.py
@@ -22,7 +22,7 @@ from nanocompore.common import NanocomporeError
 # Init randon seed
 np.random.seed(42)
 
-def txCompare(ref_pos_list, methods=None, sequence_context=0, min_coverage=20, logger=None, ref=None, sequence_context_weights="uniform", anova=True, logit=False):
+def txCompare(ref_pos_list, methods=None, sequence_context=0, min_coverage=20, logger=None, ref=None, sequence_context_weights="uniform", anova=True, logit=False, strict=True):
 
     if sequence_context_weights != "uniform" and sequence_context_weights != "harmonic":
         raise NanocomporeError("Invalid sequence_context_weights (uniform or harmonic)")
@@ -64,7 +64,7 @@ def txCompare(ref_pos_list, methods=None, sequence_context=0, min_coverage=20, l
                     tests.add("{}_intensity_pvalue".format(met))
                     tests.add("{}_dwell_pvalue".format(met))
                 elif met == "GMM":
-                    gmm_results = gmm_test(data, anova=anova, logit=logit)
+                    gmm_results = gmm_test(data, anova=anova, logit=logit, strict=strict)
                     res["GMM_model"] = gmm_results['gmm']
                     if anova:
                         res["GMM_pvalue"] = gmm_results['anova']['pvalue']
@@ -140,7 +140,7 @@ def nonparametric_test(condition1_intensity, condition2_intensity, condition1_dw
     return(pval_intensity, pval_dwell)
 
 
-def gmm_test(data, anova=True, logit=False, log_dwell=True, verbose=True):
+def gmm_test(data, anova=True, logit=False, log_dwell=True, verbose=True, strict=True):
     # Condition labels
     condition_labels = tuple(data.keys()) 
     # List of sample labels
@@ -176,7 +176,7 @@ def gmm_test(data, anova=True, logit=False, log_dwell=True, verbose=True):
             counters[lab] = Counter(y_pred[[i==lab for i in Y]])
         
         if anova:
-            aov_results = gmm_anova_test(counters, sample_condition_labels, condition_labels, gmm_ncomponents)
+            aov_results = gmm_anova_test(counters, sample_condition_labels, condition_labels, gmm_ncomponents, strict=strict)
         else:
             anova_results=None
 
@@ -212,7 +212,7 @@ def fit_best_gmm(X, max_components=2, cv_types=['spherical', 'tied', 'diag', 'fu
                 best_gmm_ncomponents = n_components
     return((best_gmm, best_gmm_type, best_gmm_ncomponents))
 
-def gmm_anova_test(counters, sample_condition_labels, condition_labels, gmm_ncomponents):
+def gmm_anova_test(counters, sample_condition_labels, condition_labels, gmm_ncomponents, strict=True):
     labels= []
     logr = []
     for sample,counter in counters.items():
@@ -224,16 +224,33 @@ def gmm_anova_test(counters, sample_condition_labels, condition_labels, gmm_ncom
         total = sum(ordered_counter)
         normalised_ordered_counter = [ i/total for i in ordered_counter ]
         # Loop through ordered_counter and divide each value by the first
-        logr.append(np.log(normalised_ordered_counter[0]/(1-normalised_ordered_counter[0]))) 
-    logr = np.array(logr)
+        logr.append(np.log(normalised_ordered_counter[0]/(1-normalised_ordered_counter[0])))
+    logr = np.around(np.array(logr), decimals=9)
     logr_s1 = [logr[i] for i,l in enumerate(labels) if l==condition_labels[0]]
     logr_s2 = [logr[i] for i,l in enumerate(labels) if l==condition_labels[1]]
-    aov_table = f_oneway(logr_s1, logr_s2)
-    aov_pvalue = aov_table.pvalue
-    # When the within variance is 0, the p-value is 0
-    # If this happens it means that the replicates have exactly the same number of points in each cluster
-    # In these cases we force the p-value to a very small number
-    if aov_pvalue == 0: aov_pvalue = np.finfo(np.float).tiny
+    # If the SS for either array is 0, skip the anova test
+    if np.sum(logr_s1-np.mean(logr_s1)) == 0 or np.sum(logr_s2-np.mean(logr_s2)):
+        if strict: 
+            raise NanocomporeError("While doing the Annova test we found a sample with within variance = 0. Use strict=False to ignore.")
+        else:
+            aov_table = "Within variance is 0"
+            aov_pvalue = np.finfo(np.float).tiny
+    else:
+        with warnings.catch_warnings():
+            # Convert warnings to errors in order to catch them
+            warnings.filterwarnings('error')
+            try:
+                aov_table = f_oneway(logr_s1, logr_s2)
+                aov_pvalue = aov_table.pvalue
+            except RuntimeWarning:
+                if strict:
+                    raise NanocomporeError("While doing the Anova test a runtime warning was raised. Use strict=False to ignore.")
+                else:
+                    warnings.filterwarnings('ignore')
+                    aov_table = f_oneway(logr_s1, logr_s2)
+                    aov_pvalue = np.finfo(np.float).tiny
+    if aov_pvalue == 0: 
+        raise NanocomporeError("The Anova test returned a p-value of 0. This is most likely an error somewhere")
     # Calculate the delta log odds ratio, i.e. the difference of the means of the log odds ratios between the two conditions
     aov_delta_logit=float(np.mean(logr_s1)-np.mean(logr_s2))
     cluster_counts = count_reads_in_cluster(counters)