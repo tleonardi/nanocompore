536ee3d6ed7ccaa9ebf2b412fb7744dac433b978
Tommaso Leonardi <tom@itm6.xyz>
2019-06-26 12:22:17 +0200

    Fixed CLI entrypoint bug

--- nanocompore/__init__.py
+++ nanocompore/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # Define self package variable
-__version__ = "1.0.0rc3"
+__version__ = "1.0.0rc3-1"
 __description__ = 'Software package that identifies raw signal changes between two conditions from https://github.com/jts/nanopolish resquiggled dRNA-Seq data.'
 #__all__ = ["SampComp", "SampCompDB", "SimReads", "Whitelist", "TxComp"]
